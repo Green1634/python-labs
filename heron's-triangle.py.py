723bf04c931d3657dbe34b6567b576d1eb0166a4
unknown <greenreina01@gmail.com>
2022-03-03 22:29:12 -0500

    heron-triangle

---
+++ heron's-triangle.py.py
@@ -0,0 +1,20 @@
+﻿def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
+
+
+a = float(input('Enter the first side\'s length: '))
+b = float(input('Enter the second side\'s length: '))
+c = float(input('Enter the third side\'s length: '))
+
+def heron(a, b, c):
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+
+def area_of_triangle(a, b, c):
+    if not is_a_triangle(a, b, c):
+        return None
+    return heron(a, b, c)
+
+
+print(area_of_triangle(a, b, c ** .5))
